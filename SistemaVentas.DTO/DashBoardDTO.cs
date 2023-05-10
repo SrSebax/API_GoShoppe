8fed427a5478f080afd31f0c823223eab273a7cd
SebasLon <111817271+SrSebax@users.noreply.github.com>
2023-05-09 19:24:34 -0500

    Sebas video 04

---
+++ SistemaVentas.DTO/DashBoardDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentas.DTO
+{
+    public class DashBoardDTO
+    {
+        public int? TotalVentas { get; set; }
+
+        public string? TotalIngresos { get; set; }
+
+        public List<VentasSemanaDTO> VentasUltimaSemana { get; set; }
+    }
+}
