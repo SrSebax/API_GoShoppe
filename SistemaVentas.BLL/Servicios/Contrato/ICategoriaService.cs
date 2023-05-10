1fde63f327bda8a694a34bbc7f9cd0c43fd6550d
S4nt1a90 <santiagomf2702@gmail.com>
2023-05-09 22:29:13 -0500

    Minuto 40:10 crear producto

---
+++ SistemaVentas.BLL/Servicios/Contrato/ICategoriaService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVentas.DTO;
+
+namespace SistemaVentas.BLL.Servicios.Contrato
+{
+    public interface ICategoriaService
+    {
+        Task<List<CategoriaDTO>> Lista();
+
+    }
+}
